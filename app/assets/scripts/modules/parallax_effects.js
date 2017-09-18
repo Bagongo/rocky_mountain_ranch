442848b8b78837df5eedfdb0031b8c20e12029d6
Ale <akak@hotmail.it>
2017-09-19 00:58:51 +0200

    Reorganized general structure. Mixins retargeted. Added responsive styling for home section

--- app/assets/scripts/modules/parallax_effects.js
+++ app/assets/scripts/modules/parallax_effects.js
@@ -1,10 +1,14 @@
+var parallaxSpeed = .1;
+
 $(".parallax-fx").each(function(){
 	var $section = $(this);
+	var $content = $(this).find(".section__content");
 	//make sure the parallax acts on a layout already set 
 	//by forcing resize and scroll before triggering the effect
 	$(window).trigger('resize').trigger('scroll');
 	$(window).scroll(function(){
-		var newPos = - ($(window).scrollTop() - $section.position().top) * 0.1;
-		$section.css("background-position", "center " + newPos + "px");		
+		var newPos = - ($(window).scrollTop() - $section.position().top) * parallaxSpeed;
+		$section.css("background-position", "center " + newPos + "px");
+		$content.css("top", - newPos + "px");		
 	});
 });
--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -10375,14 +10375,18 @@ $(".navbar-toggle").on("click", function (event) {
 "use strict";
 
 
+var parallaxSpeed = .1;
+
 $(".parallax-fx").each(function () {
 	var $section = $(this);
+	var $content = $(this).find(".section__content");
 	//make sure the parallax acts on a layout already set 
 	//by forcing resize and scroll before triggering the effect
 	$(window).trigger('resize').trigger('scroll');
 	$(window).scroll(function () {
-		var newPos = -($(window).scrollTop() - $section.position().top) * 0.1;
+		var newPos = -($(window).scrollTop() - $section.position().top) * parallaxSpeed;
 		$section.css("background-position", "center " + newPos + "px");
+		$content.css("top", -newPos + "px");
 	});
 });
 
