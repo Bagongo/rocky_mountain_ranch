9db57d64d66279302971726d4ce52d8a6f5243ba
Ale <akak@hotmail.it>
2017-10-01 18:40:28 +0200

    Added smaller images targeting mobile. Several styling fixes.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -11,18 +11,18 @@ gulp.task("previewDist", function(){
 	browserSync.init({
 	    notify: false,
 	    server: {
-	      baseDir: "dist"
+	      baseDir: "docs"
 	    }
 	});
 });
 
 gulp.task("deleteDistFolder", function(){
-	return del("./dist");
+	return del("./docs");
 });
 
 gulp.task("moveFonts", ["deleteDistFolder"], function(){
 	return gulp.src("./app/assets/fonts/**/*").
-		pipe(gulp.dest("./dist/assets/fonts"));
+		pipe(gulp.dest("./docs/assets/fonts"));
 });
 
 gulp.task("optimizeImages", ["deleteDistFolder"], function(){
@@ -32,7 +32,7 @@ gulp.task("optimizeImages", ["deleteDistFolder"], function(){
 			interlaced: true,
 			multipass: true
 		}))
-		.pipe(gulp.dest("./dist/assets/images"));
+		.pipe(gulp.dest("./docs/assets/images"));
 });
 
 gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function(){
@@ -41,7 +41,7 @@ gulp.task("usemin", ["deleteDistFolder", "styles", "scripts"], function(){
 		css: [function(){return rev();}, function(){return cssnano();}],
 		js: [function(){return rev();}, function(){return uglify();}]
 	}))
-	.pipe(gulp.dest("./dist"));
+	.pipe(gulp.dest("./docs"));
 });
 
 gulp.task("build", ["deleteDistFolder", "moveFonts", "optimizeImages", "usemin"]); 
